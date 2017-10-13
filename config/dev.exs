2ff411cb8b4c9f3516c3f58c519e66aceb43d703
Marcelo De Polli <marcelo.polli@gmail.com>
2017-10-12 21:09:31 -0300

    Add database credentials for dev and test

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :livedub, Livedub.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "marcelo",
+  password: "",
   database: "livedub_dev",
   hostname: "localhost",
   pool_size: 10
--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :livedub, Livedub.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "marcelo",
+  password: "",
   database: "livedub_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
