dc4ae14cec3c65023177ff8be3fe58e6292ef663
Marcelo De Polli <marcelo.polli@gmail.com>
2017-10-16 23:06:35 -0200

    Working /sessions response with token

--- lib/livedub_web/controllers/session_controller.ex
+++ lib/livedub_web/controllers/session_controller.ex
@@ -7,9 +7,9 @@ defmodule LivedubWeb.SessionController do
 
   def create(conn, %{"email" => email, "password" => _password}) do
     with user <- Accounts.get_user_by_email!(email) do
+      {:ok, jwt, _claims} = Livedub.Guardian.encode_and_sign(user, %{}, token_type: "access", token_ttl: {1, :month})
       conn
-      |> Livedub.Guardian.Plug.sign_in(user)
-      |> send_resp(:no_content, "")
+      |> render("create.json", user: user, jwt: jwt)
     end
   end
 
--- lib/livedub_web/views/session_view.ex
+++ lib/livedub_web/views/session_view.ex
@@ -1,3 +1,14 @@
 defmodule LivedubWeb.SessionView do
   use LivedubWeb, :view
+
+  def render("create.json", %{user: user, jwt: jwt}) do
+    %{
+      status: :ok,
+      data: %{
+        token: jwt,
+        email: user.email
+      },
+      message: "You are successfully signed in! Add this token to authorization header to make authorized requests."
+    }
+  end
 end
