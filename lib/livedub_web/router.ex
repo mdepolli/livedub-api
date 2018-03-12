c2da6196a0437d834c0650d193dfd236fc894e53
Marcelo De Polli <marcelo.polli@gmail.com>
2018-03-11 23:27:28 -0300

    Delete controllers

--- lib/livedub_web/router.ex
+++ lib/livedub_web/router.ex
@@ -21,22 +21,11 @@ defmodule LivedubWeb.Router do
     plug(LivedubWeb.Context)
   end
 
-  # scope "/", LivedubWeb do
-  #   pipe_through(:browser)
-
-  #   get("/", PageController, :index)
-  # end
-
   scope "/api" do
     pipe_through(:api)
     pipe_through(:authorized)
 
     forward("/", Absinthe.Plug, schema: LivedubWeb.Schema)
-
-    delete("/sessions", SessionController, :destroy)
-    resources("/users", UserController, only: [:show])
-    resources("/jams", JamController, only: [:index, :create, :show, :update])
-    resources("/clips", ClipController, only: [:index, :create, :show])
   end
 
   forward(
